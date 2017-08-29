aae8a4285a915cfc7f6b0ed846b53a8b1f8614ee
esirK <esirkings@gmail.com>
2017-08-29 18:45:29 +0300

    [CHORE] Create Item Model

---
+++ app/item.py
@@ -0,0 +1,6 @@
+class Item:
+    def __init__(self, name, category, price, quantity):
+        self.name = name
+        self. category = category
+        self. price = price
+        self.quantity = quantity
