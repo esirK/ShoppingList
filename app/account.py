04306a97cdeaad2336c52b00c351f8c86546d6cf
esirK <esirkings@gmail.com>
2017-08-29 13:25:06 +0300

    [CHORE] Create Account class to hold shopping list app users

---
+++ app/account.py
@@ -0,0 +1,17 @@
+class Account:
+    """ Creates an Account where users can be stored"""
+
+    def __init__(self):
+        self.users = {}
+
+    def create_user(self, user):
+        """ This Method Creates a User and adds him/her into dict of users """
+        self.users.update({user.name: user})
+
+    def rm_user(self, email):
+        """ This Method removes a user from users dictionary using his/her 
+        unique email"""
+        self.users.pop(email)
+
+    def all_users(self):
+        return self.users
