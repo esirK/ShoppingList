38ef058d452f0b3aab495540b9e09efcdc841565
esirK <esirkings@gmail.com>
2017-08-29 13:23:05 +0300

    [CHORE] Create user class to model a shopping_list user

---
+++ app/user.py
@@ -0,0 +1,18 @@
+class User:
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.shopping_lists = {}
+
+    def create_shopping_lst(self, shopping_lst):
+        """' This Method adds shopping_list into the current user shopping lists"""
+        self.shopping_lists.update({shopping_lst.name: shopping_lst})
+
+    def delete_shopping_list(self, shopping_lst_name):
+        """' This Method removes a shopping_list from the current user shopping lists"""
+        self.shopping_lists.pop(shopping_lst_name)
+
+    def get_shopping_lst(self, shopping_lst_name):
+        """ Returns a shopping_list by name the name of 'shopping_lst_name' """
+        return self.shopping_lists[shopping_lst_name]
