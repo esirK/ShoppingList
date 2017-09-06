6a446fadd888ad3dc5069daf2c5d057c820b8b75
esirK <esirkings@gmail.com>
2017-09-06 08:43:16 +0300

    Refactor html code for a better UI

--- app/models/item.py
+++ app/models/item.py
@@ -8,3 +8,13 @@ class Item:
             self.category = "General"
         else:
             self.category = category
+
+    def __eq__(self, other):
+        if self.name == other.name and self.price == other.price and \
+                        self.quantity == other.quantity and self.category == other.category:
+            return True
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
